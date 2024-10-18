fcf5c5b901eec49e5d457c99050b672c45a7fb28
GuRodri <gustavo_rodrigues_silva@hotmail.com>
2024-10-18 10:25:44 -0300

    Retornando código anterior

--- flutter_figuras_geometricas/lib/views/entrada_circulo.dart
+++ flutter_figuras_geometricas/lib/views/entrada_circulo.dart
@@ -3,18 +3,21 @@ import 'package:flutter/material.dart';
 import '../controllers/circulo_controller.dart';
 import 'resultado_circulo.dart';
 
-class EntradaCirculoPage extends StatelessWidget {
-  EntradaCirculoPage({super.key});
 
+class EntradaCirculoPage extends StatelessWidget {
+   EntradaCirculoPage({super.key});
+
+  //1. Inicializar o controller.
   final CirculoController retcontroller = CirculoController();
+
+  // 2. Declarar componentes TextEditingController para capturar
+  // os dados do circulo.
   final TextEditingController raioController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Entrada de Dados: Círculo'),
-      ),
+      appBar: AppBar(title: const Text('Entrada de Dados: Circulo'),),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -24,27 +27,20 @@ class EntradaCirculoPage extends StatelessWidget {
               decoration: const InputDecoration(labelText: "Raio:"),
               keyboardType: TextInputType.number,
             ),
-            const SizedBox(height: 20), // Espaço entre o campo e o ícone
-            const Icon(
-              Icons.circle,
-              size: 100, // Tamanho do ícone
-              color: Colors.teal, // Cor do ícone
-            ),
-            const SizedBox(height: 20), // Espaço entre o ícone e o botão
             ElevatedButton(
               onPressed: () {
-                double raio = double.parse(raioController.text);
+                double raio = double.parse(raioController.text);                
                 retcontroller.setDimensoes(raio);
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => ResultadoCirculoPage(retcontroller)),
+                  MaterialPageRoute(builder: (context) => ResultadoCirculoPage (retcontroller)),
                 );
               },
               child: const Text("Calcular"),
-            ),
+              )
           ],
         ),
-      ),
+      )
     );
   }
 }
